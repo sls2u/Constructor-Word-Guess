175f1b8d2463024ba55c6806a6837bce22a89c33
sls2u <sarahstewartvsp@gmail.com>
2018-05-01 00:12:47 -0400

    added prompts

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 //The file containing the logic for the course of the game, which depends on Word.js and:
 // Randomly selects a word and uses the Word constructor to store it
 // Prompts the user for each guess and keeps track of the user's remaining guesses
-// Letter.js should not require any other files.
-// Word.js should only require Letter.js
+
 
 var inquirer = require('inquirer');
-var Letter = require("./index.js");
+var Word = require("./word.js");
+
+//global variable:
+var outputWord =  new Word (this.outputWord);
+
 
 inquirer.prompt([
   {
-    name: "name",
-    message: "What is your name?"
-  // }, {
-  //   name: "position",
-  //   message: "What is your current position?"
-  // }, {
-  //   name: "age",
-  //   message: "How old are you?"
-  // }, {
-  //   name: "language",
-  //   message: "What is your favorite programming language?"
-   }
+    name: "start message",
+    message: "Hello! What is your name?"
+   }, {
+    name: "question",
+    type: "confirm",
+    message: 'Are you ready to guess the word?'
+  },
 ]).then(function(answers) {
-  // initializes the variable newProgrammer to be a programmer object which will take
-  // in all of the user's answers to the questions above
-  // var newProgrammer = new Programmer(answers.name, answers.position, answers.age, answers.language);
-  // // printInfo method is run to show that the newProgrammer object was successfully created and filled
-  // newProgrammer.printInfo();
+    if(answers.question === true){
+      console.log('Get Ready! Let\'s Play!');
+      inputGuess();
 
-  console.log(answers.name);
+    } else {
+      console.log('Aww shucks see ya next time...')
+    }
 });
+
+function inputGuess() {
+  inquirer.prompt([
+    {
+      name: "guess",
+      type: "input",
+      message: "Please guess a Letter"
+    }
+  ]).then(function(answers){
+    console.log(answers);
+    console.log(outputWord);
+    inputGuess();
+  });
+
+}
+
+// function outputWord(){
+//   console.log('yo');
+// }
--- word.js
+++ word.js
@@ -8,23 +8,26 @@
 var Letter = require("./letter.js");
 var inquirer = require('inquirer');
 
+
 function Word(nextWord) {
-// An array of new Letter objects representing the letters of the underlying word
-this.characters = [];
-this.nextWord = nextWord
-for (var i = 0; i < this.nextWord.length; i++) {
-  var testLetter = new Letter (this.nextWord[i]);
-  this.characters.push(testLetter);
-}
-// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-this.revealWord = function(){
-  for (var i = 0; i < this.nextWord.length; i++) {
-    var addedChar = new Letter(this.nextWord[i]);
-    this.characters.push(addedChar);
+  // An array of new Letter objects representing the letters of the underlying word
+  this.characters = [];
+  this.outputWord = ['java', 'software+engieer', 'googlefu', 'python', 'constructor']
+  for (var i = 0; i < this.outputWord.length; i++) {
+    var testLetter = new Letter(this.outputWord[i]);
+    this.characters.push(testLetter);
   }
+  // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+  this.revealWord = function() {
+    for (var i = 0; i < this.outputWord.length; i++) {
+      var addedChar = new Letter(this.outputWord[i]);
+      this.characters.push(addedChar);
+    }
+  }
+
+
 }
-}
-var testWord =  new Word ('sarahstewart');
-console.log(testWord);
+// var testWord =  new Word ([]);
+// console.log(testWord);
 // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
 module.exports = Word;
